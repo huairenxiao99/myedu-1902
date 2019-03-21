b6e536bb6fe51077f1fecfba7dbe83cfac56d102
aa <1285171232@qq.com>
2019-03-21 15:40:56 +0800

    first commit

---
+++ day01/base_type.py
@@ -0,0 +1,72 @@
+# 这是一个注释
+# model 模块
+# main 主要的
+# print 打印
+# def 生命方法
+# int    整数
+# demo 列子
+# return
+# 代码段层级关系 通过 缩进来表示
+# def int_demo():
+#     aint = 1
+#     print(aint)
+
+
+# def add(aint, bint):
+#     print(aint)
+#     print(bint)
+#     return aint + bint
+
+
+# if __name__ == '__main__':
+#     #    int_demo()
+#     add1 = add(1, 2)
+#     print(add1)
+# pass
+
+#
+# def int_demo():
+#     aint = 1
+#     print(aint)
+
+
+#  声明一个add 方法 参数有两个 aint,bint
+# def add(aint, bint):
+    # 打印aint 参数
+    # print(aint)
+    # 打印bint 参数
+    # print(bint)
+    # 返回 aint+bint
+    # return aint + bint
+
+
+# if __name__ == '__main__':
+    # int_demo()
+    # 提取变量 ctrl+alt+v
+    # 调用add方法 传入1,2 得到返回值，赋值给result变量
+    # add2 = add(1, 2)
+    # print(add2)
+
+
+def int_demo():
+    aint = 5
+    print(aint)
+
+
+def add(aint, bint):
+    print(aint)
+    print(bint)
+    return aint - bint
+def float_demo():
+    afloat = 1.20
+    print(afloat)
+    print(type(afloat))
+
+if __name__ == '__main__':
+    # aa = 1.2
+    # print(aa)
+    # print(type(aa))
+    float_demo()
+    # add3 = add(5, 1)
+    # print(add3)
+    pass
